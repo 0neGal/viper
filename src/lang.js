34cf8cbd3afb57d279f4acfb6f2f891e974137bd
0neGal <mail@0negal.com>
2021-12-29 00:58:35 +0100

    fallback to localizations
    If a message is not localized it'll default back to English, instead of
    just giving back the string...

--- src/lang.js
+++ src/lang.js
@@ -1,6 +1,7 @@
 const fs = require("fs");
 
 var lang = "en";
+var langDef = JSON.parse(fs.readFileSync(__dirname + `/lang/en.json`, "utf8"));
 if (fs.existsSync("viper.json")) {
 	lang = JSON.parse(fs.readFileSync("viper.json", "utf8")).lang;
 	if (! lang) {lang = "en"}
@@ -19,6 +20,10 @@ module.exports = (string) => {
 	if (langObj[string]) {
 		return langObj[string];
 	} else {
-		return string
+		if (langDef[string]) {
+			return langDef[string];
+		} else {
+			return string;
+		}
 	}
 }
