e1f6ac59e57c7f9dba938b7cdbbb2425b9684636
Remy Raes <contact@remyraes.com>
2022-01-01 21:43:52 +0100

    [feat] each game has its own background image

---
+++ src/app/launcher.js
@@ -0,0 +1,11 @@
+function displayVp() {
+    document.getElementById('bgHolder').style.backgroundImage = "url('../assets/bg/viper.jpg')";
+}
+
+function displayNs() {
+    document.getElementById('bgHolder').style.backgroundImage = "url('../assets/bg/northstar.jpeg')";
+}
+
+function displayTtf() {
+    document.getElementById('bgHolder').style.backgroundImage = "url('../assets/bg/ttf2.jpg')";
+}
