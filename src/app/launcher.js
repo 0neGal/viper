cab5f2eec7bd4807a686cb8bfcf19bbd2c83c3ca
Remy Raes <contact@remyraes.com>
2022-01-01 22:45:52 +0100

    [feat] buttons show related content only

--- src/app/launcher.js
+++ src/app/launcher.js
@@ -1,11 +1,28 @@
-function displayVp() {
-    document.getElementById('bgHolder').style.backgroundImage = "url('../assets/bg/viper.jpg')";
-}
+const vpContent = document.getElementById('vpContent');
+const nsContent = document.getElementById('nsContent');
+const ttfContent = document.getElementById('ttfContent');
+const bgHolder = document.getElementById('bgHolder');
 
-function displayNs() {
-    document.getElementById('bgHolder').style.backgroundImage = "url('../assets/bg/northstar.jpeg')";
-}
-
-function displayTtf() {
-    document.getElementById('bgHolder').style.backgroundImage = "url('../assets/bg/ttf2.jpg')";
+function displayContent (gameId) {
+    if (!['ttf', 'ns', 'vp'].includes(gameId)) throw new Error('wrong game id called');
+    switch(gameId) {
+        case 'vp':
+            vpContent.style.display = 'block';
+            nsContent.style.display = 'none';
+            ttfContent.style.display = 'none';
+            bgHolder.style.backgroundImage = "url('../assets/bg/viper.jpg')";
+            break;
+        case 'ns':
+            vpContent.style.display = 'none';
+            nsContent.style.display = 'block';
+            ttfContent.style.display = 'none';
+            bgHolder.style.backgroundImage = "url('../assets/bg/northstar.jpeg')";
+            break;
+        case 'ttf':
+            vpContent.style.display = 'none';
+            nsContent.style.display = 'none';
+            ttfContent.style.display = 'block';
+            bgHolder.style.backgroundImage = "url('../assets/bg/ttf2.jpg')";
+            break;
+    }
 }
