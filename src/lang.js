50b53994dbaf2f86529318209bf48fdb14f05315
Alystrasz <contact@remyraes.com>
2022-01-07 21:42:21 +0100

    [fix] loading up translation object each time a translation is needed

--- src/lang.js
+++ src/lang.js
@@ -1,27 +1,34 @@
 const fs = require("fs");
 
-var lang = "en";
-var langDef = JSON.parse(fs.readFileSync(__dirname + `/lang/en.json`, "utf8"));
-if (fs.existsSync("viper.json")) {
-	lang = JSON.parse(fs.readFileSync("viper.json", "utf8")).lang;
-	if (! lang) {lang = "en"}
-	if (! fs.existsSync(__dirname + `/lang/${lang}.json`)) {
-		if (fs.existsSync(__dirname + `/lang/${lang.replace(/-.*$/, "")}.json`)) {
-			lang = lang.replace(/-.*$/, "");
-		} else {
-			lang = "en";
+const enLang = JSON.parse(fs.readFileSync(__dirname + `/lang/en.json`, "utf8"));
+let lang = "en";
+var langObj = {};
+
+
+function _loadTranslation() {
+	if (fs.existsSync("viper.json")) {
+		lang = JSON.parse(fs.readFileSync("viper.json", "utf8")).lang;
+		if (! lang) {lang = "en"}
+		if (! fs.existsSync(__dirname + `/lang/${lang}.json`)) {
+			if (fs.existsSync(__dirname + `/lang/${lang.replace(/-.*$/, "")}.json`)) {
+				lang = lang.replace(/-.*$/, "");
+			} else {
+				lang = "en";
+			}
 		}
 	}
+	langObj = JSON.parse(fs.readFileSync(__dirname + `/lang/${lang}.json`, "utf8"));
 }
 
-var langObj = JSON.parse(fs.readFileSync(__dirname + `/lang/${lang}.json`, "utf8"));
 
 module.exports = (string) => {
+	_loadTranslation();
+
 	if (langObj[string]) {
 		return langObj[string];
 	} else {
-		if (langDef[string]) {
-			return langDef[string];
+		if (enLang[string]) {
+			return enLang[string];
 		} else {
 			return string;
 		}
