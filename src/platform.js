2648ec036f5bbd98d0b2b6180dacb35d72b0c864
Remy Raes <raes.remy@gmail.com>
2022-01-20 23:04:37 +0100

    [feat] get game path on origin installs

--- src/platform.js
+++ src/platform.js
@@ -1,10 +1,20 @@
 const os = require('os');
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
 
 
 // Tries to automatically retrieve Titanfall2 installation folder.
-function getGameFolder() {
+async function getGameFolder() {
     switch(os.platform()) {
         case 'win32':
+            const { stdout, stderr } = await exec("Get-Item -Path Registry::HKEY_LOCAL_MACHINE\\SOFTWARE\\Respawn\\Titanfall2\\", {"shell":"powershell.exe"});
+            const originPath = stdout.split('\n')
+                .filter(r => r.indexOf('Install Dir') !== -1)[0]
+                .replace(/\s+/g,' ')
+                .trim()
+                .replace('Install Dir : ','');
+            console.log(originPath);
+            
             // TODO origin => Get-Item -Path Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Respawn\Titanfall2\
             // TODO steam => 
             break;
