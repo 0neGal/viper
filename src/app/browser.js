8747588222f1bd434af802262b1479ef5b606097
0neGal <mail@0negal.com>
2022-01-21 01:23:50 +0100

    actually loads thunderstore now

--- src/app/browser.js
+++ src/app/browser.js
@@ -1,22 +1,39 @@
-function Browser(state) {
-	if (state) {
-		overlay.classList.add("shown")
-		browser.classList.add("shown")
-		return
-	} else if (! state) {
-		if (state != undefined) {
-			overlay.classList.remove("shown")
-			browser.classList.remove("shown")
+var Browser = {
+	toggle: (state) => {
+		if (state) {
+			overlay.classList.add("shown")
+			browser.classList.add("shown")
 			return
+		} else if (! state) {
+			if (state != undefined) {
+				overlay.classList.remove("shown")
+				browser.classList.remove("shown")
+				return
+			}
+		}
+
+		overlay.classList.toggle("shown")
+		browser.classList.toggle("shown")
+	},
+	loadfront: async () => {
+		let packages = await (await fetch("https://northstar.thunderstore.io/api/v1/package/")).json();
+		
+		for (let i in packages) {
+			let pkg = {...packages[i], ...packages[i].versions[0]};
+
+			new BrowserEl({
+				title: pkg.name,
+				image: pkg.icon,
+				author: pkg.owner,
+				description: pkg.description
+			})
 		}
 	}
-
-	overlay.classList.toggle("shown")
-	browser.classList.toggle("shown")
-};Browser()
+}; Browser.toggle()
+Browser.loadfront()
 
 document.body.addEventListener("keyup", (e) => {
-	if (e.key == "Escape") {Browser(false)}
+	if (e.key == "Escape") {Browser.toggle(false)}
 })
 
 function BrowserEl(properties) {
@@ -36,6 +53,8 @@ function BrowserEl(properties) {
 			<div class="text">
 				<div class="title">${properties.title}</div>
 				<div class="description">${properties.description} - by ${properties.author}</div>
+				<button>Install</button>
+				<button>Info</button>
 			</div>
 		</div>
 	`
